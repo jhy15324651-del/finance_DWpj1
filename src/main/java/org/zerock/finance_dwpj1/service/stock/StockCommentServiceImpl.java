8de38694d795b3217a197fd96b5c83eb5ecf8aca
KimKyeongTae-meta <axzy123654@gmail.com>
2025-12-04 15:38:46 +0900

    주식 게시판 댓글 서비스

---
+++ src/main/java/org/zerock/finance_dwpj1/service/stock/StockCommentServiceImpl.java
@@ -0,0 +1,86 @@
+package org.zerock.finance_dwpj1.service.stock;
+
+
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.zerock.finance_dwpj1.dto.stock.StockCommentDTO;
+import org.zerock.finance_dwpj1.entity.stock.StockBoard;
+import org.zerock.finance_dwpj1.entity.stock.StockComment;
+import org.zerock.finance_dwpj1.repository.stock.StockCommentRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class StockCommentServiceImpl implements StockCommentService {
+
+    private final StockCommentRepository stockCommentRepository;
+
+    private StockComment dtoToEntity(StockCommentDTO dto) {
+
+        return StockComment.builder()
+                .id(dto.getId())
+                .board(StockBoard.builder()
+                        .id(dto.getBoardId())
+                        .build())
+                .writer(dto.getWriter())
+                .content(dto.getContent())
+                .regDate(dto.getRegDate())
+                .modDate(dto.getModDate())
+                .build();
+    }
+
+    private StockCommentDTO entityToDto(StockComment comment) {
+
+        return StockCommentDTO.builder()
+                .id(comment.getId())
+                .boardId(comment.getBoard().getId())
+                .writer(comment.getWriter())
+                .content(comment.getContent())
+                .regDate(comment.getRegDate())
+                .modDate(comment.getModDate())
+                .build();
+    }
+
+    @Override
+    public Long register(StockCommentDTO dto) {
+        StockComment comment = dtoToEntity(dto);
+        StockComment saved = stockCommentRepository.save(comment);
+        return saved.getId();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<StockCommentDTO> getListByBoard(Long boardId){
+        return stockCommentRepository.findByBoard_IdOrderByIdAsc(boardId)
+                .stream()
+                .map(this::entityToDto)
+               .collect(Collectors.toList());
+    }
+
+    @Override
+    public void modify(StockCommentDTO dto){
+        StockComment comment = stockCommentRepository.findById(dto.getId())
+                .orElseThrow(()-> new IllegalArgumentException("댓글이 없습니다."));
+        comment.setContent(dto.getContent());
+        comment.setModDate(LocalDateTime.now());
+
+        stockCommentRepository.save(comment);
+    }
+
+    @Override
+    public void remove(Long id){
+        stockCommentRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public int getCountByBoard(Long boardId){
+        return stockCommentRepository.countByBoard_Id(boardId);
+    }
+
+}
